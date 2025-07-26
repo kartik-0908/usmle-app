2cf2aa216360ad51b10736b93587aedc195cbc13
Kartik Agarwal <kartik_a@mt.iitr.ac.in>
2025-07-27 04:02:00 +0530

    optimised db query

--- app/dashboard/practice/[step]/page.tsx
+++ app/dashboard/practice/[step]/page.tsx
@@ -1,17 +1,30 @@
+import { auth } from "@/app/lib/auth";
 import { TopicCards } from "@/components/section-cards";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ step: string }>;
 }) {
+  const now = Date.now();
+  console.log("started fetching steps slug and userId", now);
   const { step } = await params;
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/sign-in");
+  }
+  const userId = session.user.id;
+  console.log("userId fetched at", Date.now() - now);
 
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <TopicCards step={step} />
+          <TopicCards step={step} userId={userId} />
         </div>
       </div>
     </div>
